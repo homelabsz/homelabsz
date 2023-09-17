e63f5bd852dbfc1c9c005e5937d1ddd4852d5e0f
lpsm-dev-github <lpsm-dev@protonmail.com>
2023-09-17 11:45:12 -0300

    fix: setup new projects

---
+++ 0-infrastructure/2-cloud/aws/5-setup-instances/bastion-windows/_data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "windows_2019" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base*"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
