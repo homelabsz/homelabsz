70205b94afc68ce6d019003714e02dd7ed8430a7
lpsm-dev-github <lpsm-dev@protonmail.com>
2023-09-23 09:40:30 -0300

    chore: add new files

---
+++ 0-infrastructure/2-cloud/oracle/_variables.tf
@@ -0,0 +1,25 @@
+variable "tenancy_ocid" {
+  description = "The OCID of the tenancy."
+  type        = string
+}
+
+variable "user_ocid" {
+  description = "The OCID of the user."
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "The location of the private key."
+  type        = string
+}
+
+variable "fingerprint" {
+  description = "The fingerprint of the private key."
+  type        = string
+}
+
+variable "region" {
+  description = "The name of the cloud region."
+  type        = string
+  default     = "ap-sydney-1"
+}
